0753a163307743d1430a24c925d60c207568fd45
Yokk1e <satthabut.o@gmail.com>
2021-03-14 17:11:53 +0700

    - [feat] add dotenv

--- main.js
+++ main.js
@@ -1,25 +1,26 @@
 const express = require("express");
-const db = require('./db')
+require("dotenv").config();
+const db = require("./db");
 const app = express();
 const PORT = process.env.PORT || 8080;
 
 app.get("/", (req, res) => {
   const linkData = {
-    link : "test najea"
-  }
+    link: "test najea",
+  };
 
-  db.collection('links').doc('RkS1KrSAZxLsBXBiu7JH').set(linkData)
+  db.collection("links").doc().set(linkData);
+
   res.send("Talesrunner discord bot start!");
-
 });
 
-app.get("/test",async (req,res)=>{
-  const test = await db.collection('links').get()
-  test.forEach(doc=>{
-    console.log(doc.data)
-  })
-  res.json({tte:'test'});
-})
+app.get("/test", async (req, res) => {
+  const test = await db.collection("links").get();
+  test.forEach((doc) => {
+    console.log(doc.data);
+  });
+  res.json({ tte: "test" });
+});
 
 app.listen(PORT, () => {
   console.log(`Server run on port ${PORT}`);
