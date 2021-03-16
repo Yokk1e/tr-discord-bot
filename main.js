8ad007a5b54250cde383dd2fcd31cd9cd7c213e4
Yokk1e <satthabut.o@gmail.com>
2021-03-16 10:20:48 +0700

    - [feat] update route

--- main.js
+++ main.js
@@ -14,37 +14,54 @@ const webhookClient = new Discord.WebhookClient(
   process.env.WEBHOOK_TOKEN
 );
 
+app.get("/", (req, res) => {
+  res.send("hello yokk1e");
+});
+
+app.get("/get-news/trigger", async (req, res) => {
+  try {
+    await triggerNews();
+  } catch (error) {
+    console.log(error);
+  }
+
+  res.send("success");
+});
+
+async function triggerNews() {
+  const currentLinks = await scraping.getNews();
+
+  let oldLinks = await db.collection("links").get();
+
+  let oldLinksArray = [];
+  oldLinks.forEach((link) => {
+    oldLinksArray.push(link.data().link);
+  });
+  const links = currentLinks.filter((link) => {
+    return oldLinksArray.indexOf(link) === -1;
+  });
+
+  for (const link of links) {
+    try {
+      await webhookClient.send(link);
+      await db.collection("links").doc().set({ link });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+}
 /*
  * running a task every 10 minutes
  */
 
-let isProcessRunning = false;
-cron.schedule("*/10 * * * *", async () => {
-  if (!isProcessRunning) {
-    isProcessRunning = true;
-    const currentLinks = await scraping.getNews();
-
-    let oldLinks = await db.collection("links").get();
-
-    let oldLinksArray = [];
-    oldLinks.forEach((link) => {
-      oldLinksArray.push(link.data().link);
-    });
-    const links = currentLinks.filter((link) => {
-      return oldLinksArray.indexOf(link) === -1;
-    });
-
-    for (const link of links) {
-      try {
-        await webhookClient.send(link);
-        await db.collection("links").doc().set({ link });
-      } catch (error) {
-        console.log(error);
-      }
-    }
-    isProcessRunning = false;
-  }
-});
+// let isProcessRunning = false;
+// cron.schedule("*/10 * * * *", async () => {
+//   if (!isProcessRunning) {
+//     isProcessRunning = true;
+//     await triggerNews();
+//     isProcessRunning = false;
+//   }
+// });
 
 app.listen(PORT, () => {
   console.log(`Server run on port ${PORT}`);
